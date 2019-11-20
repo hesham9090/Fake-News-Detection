3d2f091c85cc74b49ed62ea446fed08dd40ac331
unknown <ahmed.t.mohamed@ieee.org>
2019-11-20 02:49:42 +0200

    model update

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,12 +1,12 @@
 import re
 def Preprocess_text(data):
-    data['Title'] = data['Title'].apply(lambda x: re.sub(r'[^a-zA-Z0-9 ]',r'',x))
-    data['Title'] = data['Title'].apply(lambda x: re.sub(r'[0-9]+',r'NUM',x))
+    data['Title'] = data['Title'].apply(lambda x: re.sub(r'[^a-zA-Z0-9 ]',r' ',x))
     data['Title'] = data['Title'].apply(lambda x: x.lower())
-    data['Article'] = data['Article'].apply(lambda x: re.sub(r'[^a-zA-Z0-9 ]',r'',x))
-    data['Article'] = data['Article'].apply(lambda x: re.sub(r'[0-9]+',r'NUM',x))
+    data['Title'] = data['Title'].apply(lambda x: re.sub(r'[0-9]+',r' NUM ',x))
+    data['Article'] = data['Article'].apply(lambda x: re.sub(r'[^a-zA-Z0-9 ]',r' ',x))
     data['Article'] = data['Article'].apply(lambda x: x.lower())
-    data['Author'] = data['Author'].apply(lambda x: re.sub(r'[^a-zA-Z0-9 ]',r'',x))
-    data['Author'] = data['Author'].apply(lambda x: re.sub(r'[0-9]+',r'NUM',x))
+    data['Article'] = data['Article'].apply(lambda x: re.sub(r'[0-9]+',r' NUM ',x))
+    data['Author'] = data['Author'].apply(lambda x: re.sub(r'[^a-zA-Z0-9 ]',r' ',x))
     data['Author'] = data['Author'].apply(lambda x: x.lower())
+    data['Author'] = data['Author'].apply(lambda x: re.sub(r'[0-9]+',r' NUM ',x))
     return data
